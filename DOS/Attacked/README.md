785dce37da3e9835e82be4837cfa746a00e7b667
Sanfeire <67929659+Sanfeire@users.noreply.github.com>
2021-11-06 19:32:46 +0100

    Add files via upload

---
+++ DOS/Attacked/README.md
@@ -0,0 +1,3 @@
+# Attacked
+A repository where you can find a basic usage script to make Ddos an Dos attack to web pages, number phones, e-mails or specific ips
+--UNDER CONSTRUCTION (only the README file)--
---
+++ DOS/Attacked/tools/addons/logo.py
@@ -0,0 +1,15 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+  _________________________
+< created by @breakshacker >
+ --------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+{Fore.RESET}""")
